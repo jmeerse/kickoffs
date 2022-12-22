b03fcc9487a3f047d17f144010c833b01c25ba24
jmeerse <jmeerse@gmail.com>
2022-12-22 15:28:31 -0500

    next update

--- kickoffs.R
+++ kickoffs.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(nflreadr)
 library(ggrepel)
+library(gt)
 
 
 #load plays from one season
@@ -14,15 +15,18 @@ library(ggrepel)
 
 all_inj <- data.frame() #only run this one time, to create an empty dataframe
 
-yr <- 2001 #change this for each year (data goes from 1999 through 2022)
+yr <- 2000 #change this for each year (data goes from 1999 through 2022)
 
-plays <- load_pbp(seasons = yr) %>%
+years = c(1999:2010)
+
+for (i in years) {
+plays <- load_pbp(seasons = i) %>%
   filter(!is.na(play_type), 
          play_type != "qb_kneel",
          play_type != "qb_spike") %>% 
   group_by(season, play_type) %>% summarise(n = n())
 
-plays_inj <- load_pbp(seasons = yr) %>% 
+plays_inj <- load_pbp(seasons = i) %>% 
   filter(grepl('injured', desc), 
          !is.na(play_type), 
          play_type != "qb_kneel",
@@ -33,16 +37,19 @@ plays_all <- left_join(plays, plays_inj,
                         by = c("season" = "season", 
                                "play_type" = "play_type"))
 
-plays_all <- plays_all %>% mutate(pct_inj = (n.y / n.x)*100) #injuries per 100 plays
+plays_all <- plays_all %>% mutate(pct_inj = round((n.y / n.x)*100, 2)) #injuries per 100 plays
 
 all_inj <- rbind(all_inj, plays_all)
 #go back up, change year, run again
+gc()
+}
 
 max <- max(all_inj$season)
 
 all_inj %>% 
-  ggplot(aes(x = season, y = pct_inj, color = play_type, legend = F)) +
+  ggplot(aes(x = season, y = pct_inj, color = play_type)) +
   scale_x_continuous(breaks = scales::breaks_width(1)) +
+  guides(color = FALSE) +
   geom_line() +
   geom_label_repel(data = filter(all_inj, season == max), 
                    aes(x = season, y = pct_inj, label = play_type)) +
@@ -52,4 +59,7 @@ all_inj %>%
        title = "Injury rates by Play Type",
        subtitle = "From xx to xx")
 
+all_inj_wide <- all_inj %>% select(season, play_type, pct_inj) %>%  
+  spread(season, pct_inj)
 
+all_inj_wide %>% gt()
