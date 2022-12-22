bcc6407a1733068b0c2902f56ae05e2f12ab3882
jmeerse <jmeerse@gmail.com>
2022-12-22 14:31:06 -0500

    update 12/22

--- kickoffs.R
+++ kickoffs.R
@@ -3,63 +3,53 @@ library(tidyverse)
 library(nflreadr)
 library(ggrepel)
 
-plays %>% 
-  group_by(specialTeamsPlayType) %>% 
-  summarise(n = n())
 
-names(tracking2018)
-
-tracking2018 %>% filter(gameId == 2018123000 & playId == 36 & event == "tackle")
-ko18 <- tracking2018 %>% filter(event == "kickoff")
-kickoffs18 <- load_pbp(seasons = 2018) %>% filter(play_type == "kickoff")
-
-kos_inj18 <- kickoffs18 %>% filter(grepl('injured', desc))
-
-inj18 <- load_pbp(seasons = 2018) %>% 
-  filter(grepl('injured', desc)) %>% 
-  group_by(play_type) %>% 
-  summarise(n = n())
-
-plays18 <- load_pbp(seasons = 2018) %>% group_by(play_type) %>% summarise(n = n())
-
-plays18 <- left_join(plays18, inj18, by = "play_type")
-
-plays18 <- plays18 %>% mutate(inj_rt = n.y / n.x)
-
-
-#load plays from many seasons
+#load plays from one season
 #group by season and play_type, count types, save as dataframe
 #filter to plays with injuries in description
 #group injuries by play_type and count play_types, save as df
 #left_join to play_count df, mutate to pct of injuries by play type
+#do again for next year. rjoin 
 #graph year_by_year to see changes
 
-plays10_22 <- load_pbp(seasons = 2010:2022) %>% 
+all_inj <- data.frame() #only run this one time, to create an empty dataframe
+
+yr <- 2001 #change this for each year (data goes from 1999 through 2022)
+
+plays <- load_pbp(seasons = yr) %>%
+  filter(!is.na(play_type), 
+         play_type != "qb_kneel",
+         play_type != "qb_spike") %>% 
   group_by(season, play_type) %>% summarise(n = n())
 
-plays10_22_inj <- load_pbp(seasons = 2010:2022) %>% 
-  filter(grepl('injured', desc)) %>% 
+plays_inj <- load_pbp(seasons = yr) %>% 
+  filter(grepl('injured', desc), 
+         !is.na(play_type), 
+         play_type != "qb_kneel",
+         play_type != "qb_spike") %>% 
   group_by(season, play_type) %>% summarise(n = n())
 
-plays10_22_inj %>% filter(play_type == "kickoff" | play_type == "punt") %>% 
-  ggplot(aes(x = season, y = n, color = play_type)) +
-  geom_line()
-
-plays10_22 <- left_join(plays10_22, plays10_22_inj, 
+plays_all <- left_join(plays, plays_inj, 
                         by = c("season" = "season", 
                                "play_type" = "play_type"))
 
-plays10_22 <- plays10_22 %>% mutate(pct_inj = (n.y / n.x)*100)
+plays_all <- plays_all %>% mutate(pct_inj = (n.y / n.x)*100) #injuries per 100 plays
 
-plays10_22 %>% 
-  ggplot(aes(x = season, y = pct_inj, color = play_type)) +
+all_inj <- rbind(all_inj, plays_all)
+#go back up, change year, run again
+
+max <- max(all_inj$season)
+
+all_inj %>% 
+  ggplot(aes(x = season, y = pct_inj, color = play_type, legend = F)) +
   scale_x_continuous(breaks = scales::breaks_width(1)) +
   geom_line() +
-  geom_label_repel(data = filter(plays10_22, season == "2022"), 
-                   aes(x = season, y = pct_inj, label = play_type),
-                   nudge_x = 1,
-                   na.rm = TRUE) +
+  geom_label_repel(data = filter(all_inj, season == max), 
+                   aes(x = season, y = pct_inj, label = play_type)) +
   theme_light() +
   labs(x = "Season",
        y = "Injuries per 100 plays",
-       title = "Injury rates by Play Type")
+       title = "Injury rates by Play Type",
+       subtitle = "From xx to xx")
+
+
